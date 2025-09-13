1c99c6430b56f612afcab484e0d7496f0ea2f9bc
ARIEFSOLOMON <ARIEFSOLOMON8@GMAIL.COM>
2025-09-13 15:44:33 +0700

    tugas 3: data delivery

--- main/models.py
+++ main/models.py
@@ -1,15 +1,23 @@
+import uuid
 from django.db import models
 
 # Create your models here.
 class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('ball', 'Ball'),
+        ('shoes', 'Shoes'),
+        ('racket', 'Racket'),
+        ('safety', 'Safety'),
+    ]
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
     thumbnail = models.URLField()
-    category = models.CharField(max_length=100)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='')
+    stock = models.IntegerField(default=0)
+    brand = models.CharField(max_length=100, blank=True)
     is_featured = models.BooleanField(default=False)
-    stock = models.IntegerField(default=0)  # Atribut tambahan
-    brand = models.CharField(max_length=100, blank=True)  # Atribut tambahan
 
     def __str__(self):
         return self.name
