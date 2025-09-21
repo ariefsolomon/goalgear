ef0fae41df4d7b9470d8cfc374401d42ac749087
ariefsolomon <ariefsolomon8@gmail.com>
2025-09-21 11:29:59 +0700

    Tugas 4: Autentikasi, Session, dan Cookies

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Product(models.Model):
@@ -11,13 +12,14 @@ class Product(models.Model):
     ]
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
-    price = models.IntegerField()
+    price = models.PositiveIntegerField()
     description = models.TextField()
     thumbnail = models.URLField()
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='')
-    stock = models.IntegerField(default=0)
+    stock = models.PositiveIntegerField(default=0)
     brand = models.CharField(max_length=100, blank=True)
     is_featured = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name
